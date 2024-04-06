59a542062c6cde68bed945e473de18825f27ee3a
hammad-ist <hammad@innostark.com>
2024-04-07 01:21:47 +0500

    Added create/update/delete operations.

--- LibrarySystem.Api/LibrarySystem.Api/Controllers/AuthorController.cs
+++ LibrarySystem.Api/LibrarySystem.Api/Controllers/AuthorController.cs
@@ -1,4 +1,5 @@
 ﻿using LibrarySystem.Api.Data;
+using LibrarySystem.Api.Models.Domain;
 using LibrarySystem.Api.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
@@ -32,6 +33,59 @@ namespace LibrarySystem.Api.Controllers
             }
 
             return Ok(AuthorDto.Map(author));
+        }
+
+        [HttpPost]
+        public IActionResult Post(CreateAuthorDto author)
+        {
+            var authorToCreate = new Author
+            {
+                Name = author.Name,
+                Email = author.Email,
+                DateOfBirth = author.DateOfBirth
+            };
+
+            _applicationDbContext.Authors.Add(authorToCreate);
+            _applicationDbContext.SaveChanges();
+
+            return Ok(AuthorDto.Map(authorToCreate));
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Put(int id, CreateAuthorDto author)
+        {
+            var authorToUpdate = _applicationDbContext.Authors.Find(id);
+            if (authorToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            authorToUpdate.Name = author.Name;
+            authorToUpdate.Email = author.Email;
+            authorToUpdate.DateOfBirth = author.DateOfBirth;
+
+            _applicationDbContext.Authors.Update(authorToUpdate);
+            _applicationDbContext.SaveChanges();
+
+            return Ok(AuthorDto.Map(authorToUpdate));
+        }
+
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var authorToUpdate = _applicationDbContext.Authors.Find(id);
+            if (authorToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            _applicationDbContext.Authors.Remove(authorToUpdate);
+            _applicationDbContext.SaveChanges();
+
+            return NoContent();
         }
 
         public AuthorController(ApplicationDbContext applicationDbContext)
---
+++ LibrarySystem.Api/LibrarySystem.Api/Models/Dto/CreateAuthorDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibrarySystem.Api.Models.Dto
+{
+    public class CreateAuthorDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
