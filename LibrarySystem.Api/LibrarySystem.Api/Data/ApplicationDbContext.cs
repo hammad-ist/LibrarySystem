837b9d1ea038fa6793b90060b48cd93842b9d6cc
hammad-ist <hammad@innostark.com>
2024-04-19 23:39:54 +0500

    Added user table.

--- LibrarySystem.Api/LibrarySystem.Api/Data/ApplicationDbContext.cs
+++ LibrarySystem.Api/LibrarySystem.Api/Data/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace LibrarySystem.Api.Data
     {
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
---
+++ LibrarySystem.Api/LibrarySystem.Api/Models/Domain/User.cs
@@ -0,0 +1,11 @@
+﻿namespace LibrarySystem.Api.Models.Domain
+{
+    public class User : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
